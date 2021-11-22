5025a33f72d4ecb89b09ccb0bc8ba0a1dbcec06d
javier <javierjin@tencent.com>
2021-11-22 19:27:10 +0800

    增加nexus与sonar的部署等待前置逻辑

--- script/initBeforeJenkinsStart.sh
+++ script/initBeforeJenkinsStart.sh
@@ -1,7 +1,52 @@
 echo "waiting for nexus starting fully..."
-sleep 10s
+function waitNexus {
+   while true;    
+   do
+        nexusResponse=$(curl -w %{http_code} -s -o /dev/null $NEXUS_SCHEME://localhost:8081/service/rest/v1/status);
+        echo "Now Nexus api status code is $nexusResponse";
+        if [ $nexusResponse = "200" ];then
+               if [ ! -f $NEXUS_DEPLOY_FILE ]; then
+                    touch $NEXUS_DEPLOY_FILE
+               fi;
+               break;
+        fi;
+        echo "Waiting Nexus starting...";
+        sleep 3;
+   done
+}
+export -f waitNexus
+timeout 600s /bin/bash -c waitNexus
+
+if [ ! -f $NEXUS_DEPLOY_FILE ];then
+        echo "Nexus deploy fail(timeout)";
+        exit 1;
+fi;
+echo "Nexus deploy success..."
+
 echo "waiting for sonarqube starting fully..."
-sleep 10s
+function waitSonar {
+   while true;    
+   do
+        sonarResponse=$(curl -u admin:admin -w %{http_code} -s -o /dev/null $SONAR_SCHEME://localhost:9000/api/system/health);
+        echo "Now Sonarqube api status code is $sonarResponse";
+        if [ $sonarResponse = "200" ];then
+               if [ ! -f $SONAR_DEPLOY_FILE ]; then
+                    touch $SONAR_DEPLOY_FILE
+               fi;
+               break;
+        fi;
+        echo "Waiting Sonarqube starting...";
+        sleep 3;
+   done
+}
+export -f waitSonar
+timeout 600s /bin/bash -c waitSonar
+
+if [ ! -f $SONAR_DEPLOY_FILE ];then
+        echo "Sonarqube deploy fail(timeout)";
+        exit 1;
+fi;
+echo "Sonarqube deploy success..."
 
 #modify demo workspaceId
 su-exec jenkins /bin/bash -c  "sed -i 's/<tapdWorkspaceId>xxx<\/tapdWorkspaceId>/<tapdWorkspaceId>$TAPD_WORKSPACE_ID<\/tapdWorkspaceId>/' /data/devops_data/jenkins_home/jobs/DemoPipeline/config.xml"
