c9e31673a71bf1acd8e301d7b679f7776faa59dc
javier <javierjin@tencent.com>
2021-11-29 15:40:48 +0800

    add debug version

--- script/init.sh
+++ script/init.sh
@@ -27,20 +27,74 @@ nexusInitPwd=$(< /dev/urandom tr -dc 'A-Za-z0-9' | head -c32 )
 echo "save New Nexus Admin Password";
 curl -slL -u admin:${oldNexusPwd} -X PUT --data-raw "${nexusInitPwd}" -H "Content-Type: text/plain; charset=UTF-8" $nexusChangePwdURL
 echo $nexusInitPwd >/data/devops_data/secrets/nexusInitialAdminPassword
+echo "------------------------------------TAPD DEBUG START------------------------------------";
+echo ">>>>>>>>>>>>>>>>>>>>>>>SHOW JENKINS PASSWORD START"
+echo "jenkins password is ${JenkinsPwd}"
+echo ">>>>>>>>>>>>>>>>>>>>>>>SHOW JENKINS PASSWORD END"
 
+echo ">>>>>>>>>>>>>>>>>>>>>>>SHOW JENKINS CrumbTokenURL START"
+echo "jenkins CrumbTokenURL is ${jenkinsCrumbTokenURL}"
+echo ">>>>>>>>>>>>>>>>>>>>>>>SHOW JENKINS CrumbTokenURL END"
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>EXECUTE generating JENKINs ApiToken START"
 echo "generating ApiToken of jenkins admin account...";
-CRUMB=$(curl -slL -u "admin:${JenkinsPwd}" $jenkinsCrumbTokenURL -c cookies.txt)
+echo "=======================part1 generating JENKINs CRUMB START"
+echo "curl -vlL -u "admin:${JenkinsPwd}" $jenkinsCrumbTokenURL -c cookies.txt"
+CRUMB=$(curl -vlL -u "admin:${JenkinsPwd}" $jenkinsCrumbTokenURL -c cookies.txt)
+echo "=======================part1 generating JENKINs CRUMB END"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "=======================part2 SHOW JENKINs CRUMB START"
+echo "${CRUMB}"
+echo "=======================part2 SHOW JENKINs CRUMB END"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "=======================part3 SHOW cookies.txt START"
+cat cookies.txt
+echo "=======================part3 SHOW cookies.txt END"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "=======================part4 generating JenkinsApiToken START"
+echo "curl -slL -u \"admin:${JenkinsPwd}\" $jenkinsCreateAPITokenURL \
+-H \"${CRUMB}\" \
+--data 'newTokenName=devopsinit' \
+-b cookies.txt | sed -n -e 's/\"//gp' | sed -n -e 's/,/\n/gp'| grep tokenValue: | awk -F ':' '{print $2}'"
 
-JenkinsApiToken=$(curl -slL -u "admin:${JenkinsPwd}" $jenkinsCreateAPITokenURL \
+JenkinsApiToken=$(curl -vlL -u "admin:${JenkinsPwd}" $jenkinsCreateAPITokenURL \
 -H "${CRUMB}" \
 --data 'newTokenName=devopsinit' \
 -b cookies.txt | sed -n -e 's/"//gp' | sed -n -e 's/,/\n/gp'| grep tokenValue: | awk -F ':' '{print $2}')
+echo "=======================part4 generating JenkinsApiToken END"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "=======================part5 SHOW JenkinsApiToken START"
+echo "${JenkinsApiToken}"
+echo "JenkinsApiToken=${JenkinsApiToken%\\}\\}*}"
 JenkinsApiToken=${JenkinsApiToken%\}\}*}
+echo "${JenkinsApiToken}"
+echo "=======================part5 SHOW JenkinsApiToken END"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "=======================part6 SHOW cookies.txt START"
+cat cookies.txt
+echo "=======================part6 SHOW cookies.txt END"
+echo ">>>>>>>>>>>>>>>>>>>>>>>EXECUTE generating JENKINs ApiToken END"
 
+echo ">>>>>>>>>>>>>>>>>>>>>>>EXECUTE credential for nexus START"
 echo "generating credential for nexus...";
-CRUMB=$(curl -slL -u "admin:${JenkinsPwd}" $jenkinsCrumbTokenURL)
-curl -s -X POST -u "admin:${JenkinsApiToken}"  -H "${CRUMB}" $jenkinsCreateCredentialsURL --data-urlencode 'json={"":"0","credentials":{"scope":"GLOBAL","id":"DevOpsNexusPassword","username":"admin","password":"'"${nexusInitPwd}"'","description":"DevOpsNexusPassword","$class":"com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl"}}'
+echo "=======================part1 generating JENKINs CRUMB START"
+echo "curl -vlL -u "admin:${JenkinsPwd}" $jenkinsCrumbTokenURL"
+CRUMB=$(curl -vlL -u "admin:${JenkinsPwd}" $jenkinsCrumbTokenURL)
+echo "=======================part1 generating JENKINs CRUMB END"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "=======================part2 SHOW JENKINs CRUMB START"
+echo "${CRUMB}"
+echo "=======================part2 SHOW JENKINs CRUMB END"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "=======================part3 generating JenkinsApiToken START"
+echo "curl -v -X POST -u \"admin:${JenkinsApiToken}\"  -H \"${CRUMB}\" $jenkinsCreateCredentialsURL --data-urlencode 'json={\"\":\"0\",\"credentials\":{\"scope\":\"GLOBAL\",\"id\":\"DevOpsNexusPassword\",\"username\":\"admin\",\"password\":\"'\"${nexusInitPwd\"'\",\"description\":\"DevOpsNexusPassword\",\"$class\":\"com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl\"}}'"
+curl -v -X POST -u "admin:${JenkinsApiToken}"  -H "${CRUMB}" $jenkinsCreateCredentialsURL --data-urlencode 'json={"":"0","credentials":{"scope":"GLOBAL","id":"DevOpsNexusPassword","username":"admin","password":"'"${nexusInitPwd}"'","description":"DevOpsNexusPassword","$class":"com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl"}}'
+echo "=======================part3 generating JenkinsApiToken END"
+echo ">>>>>>>>>>>>>>>>>>>>>>>EXECUTE credential for nexus END"
+
+echo "------------------------------------TAPD DEBUG END------------------------------------";
 sleep 5;
+
 echo "adding Nexus configuration to jenkins ...";
 CRUMB=$(curl -slL -u "admin:${JenkinsPwd}" $jenkinsCrumbTokenURL)
 curl -slL -X POST -u "admin:${JenkinsApiToken}" -H "${CRUMB}" $jenkinsAddNexusCfgURL --data-urlencode "id=DevOpsNexus" --data-urlencode "displayName=DevOpsNexus" --data-urlencode "serverUrl=http://${HOST}:${NEXUS_PORT}" --data-urlencode "credentialsId=DevOpsNexusPassword"
